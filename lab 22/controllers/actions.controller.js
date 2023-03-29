3dc0540031d10895d80e1072d46f003e64fff9ec
sjimenezbauer <sjimenezbauer@gmail.com>
2023-03-29 14:04:07 -0600

    Funciona solo falta error chico

--- lab 22/controllers/actions.controller.js
+++ lab 22/controllers/actions.controller.js
@@ -1,17 +1,13 @@
 const fs = require('fs');
 const path = require('path');
 
-const uploadDir = path.join(__dirname, 'uploads');
-if (!fs.existsSync(uploadDir)) {
-  fs.mkdirSync(uploadDir);
-}
 const Actions = require('../models/actions.model');
 const Type = require('../models/type.model');
 const multer = require('multer');
 
 const fileStorage = multer.diskStorage({
   destination: (req, file, cb) => {
-      cb(null, path.join(__dirname, '.', 'uploads'));
+      cb(null, path.join(__dirname, '..', 'uploads'));
   },
   filename: (req, file, cb) => {
       cb(null, new Date().toISOString() + '-' + file.originalname);
